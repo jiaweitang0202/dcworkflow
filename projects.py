7a0d95cdcde45e31d1acb7e5639a6dba9ef950c1
Nan Tang <joe.ntang@gmail.com>
2017-08-23 16:45:16 +0300

    add

---
+++ projects.py
@@ -0,0 +1,31 @@
+#import jgraph
+from graphviz import *
+
+def newProject():
+    print ("create a new project\n")
+    dot = Digraph(comment='The Workflow Graph')
+
+    return dot
+    
+    
+def loadProject(input_file):
+    print ("load an existing project from: " + input_file + "\n")
+    
+    file = open(input_file, 'r')
+    text = file.read()
+    dot = Source(text)
+    
+    return dot
+
+
+def saveProject(dot, output_file):
+    print ("save the current project to: " + output_file + "\n")
+    dot.render(output_file)
+    
+def closeProject():
+    print ("close the current project")
+    
+def addFunction(dot):
+    dot.add_node('D', 'Golden Record')
+    #dot.addedge('C', 'D')
+    return dot
